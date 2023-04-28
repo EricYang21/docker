2597d6936f2b25bcd746a7b009a3eb7a1d327680
EricYang <vm20318@bristol.ac.uk>
2023-04-28 15:40:49 +0100

    one

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import {Link} from "react-router-dom";
+import Home from "./pages/Home";
+import List from "./pages/List";
+import Login from "./pages/Login";
+import SignUp from"./pages/Signup";
+import Comment from "./pages/Comment";
+
+function App() {
+  return (
+    <div>
+      <div className='App'>
+        <header className="header fr content">
+          <a href="https://bristolenergynetwork.org">
+            <img src="https://i.postimg.cc/KzWjzhnQ/BEN-logo-art-L-web.jpg" alt="Bristol Energy Network" height={60} />
+          </a>
+          <div className='fr sign-btn'>
+            <Link to="/signup">
+              <button className="header-btn sign-up">Sign up</button>
+            </Link>
+            <Link to="/login">
+              <button className="header-btn sign-in">Sign in</button>
+            </Link>
+          </div>
+        </header>
+      </div>
+      <div className="menu">
+        <div className="fr content">
+          <Link to="/">
+            <span className="menu_item">
+        Home
+            </span>
+          </Link>
+          <span className="menu_item">
+        About us
+          </span>
+          <span className="menu_item">
+        Contact
+          </span>
+          <div className="blank"></div>
+      
+        </div>
+      </div>
+     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/comment/:id" element={<Comment />} />
+      </Routes>
+      <div className='copyright'>
+        <div className="content">
+          <div className="fr">
+            <img src="https://i.postimg.cc/KzWjzhnQ/BEN-logo-art-L-web.jpg" alt="" width={200} />
+            <div className="fr blank">
+              <div className="blank"></div>
+              {["Contact Us","Privacy","More Information"].map((item, index) => <div style={{padding:"0 12px"}} key={index}>{item}</div>)}
+            </div>
+          </div>
+          <br />
+          <br />
+          <input placeholder="search..."/><button>Search</button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
