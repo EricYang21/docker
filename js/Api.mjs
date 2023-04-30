d7975742c35930003b296ccfc50067d24d075a2f
Eric <vm20318@bristol.ac.uk>
2023-04-30 20:42:54 +0100

    localhost改成docker_backend_1

--- js/Api.mjs
+++ js/Api.mjs
@@ -1,4 +1,4 @@
-const url = process.env.API_URL || "http://localhost:8080";
+const url = process.env.API_URL || "http://docker_backend_1:8080";
 
 export function query(sources, tasks, postcode) {
   let body = {
